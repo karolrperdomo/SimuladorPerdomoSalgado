47b2a7e5349d7110016fcf335b0ab236a3ae90ad
Karol Perdomo <karol.rperdomo@gmail.com>
2023-09-25 22:50:57 -0500

    se actualiza condicionales

--- js/main.js
+++ js/main.js
@@ -1,49 +1,23 @@
 let clave = prompt("Ingresa la contraseña: ");
 
-// repetir hasta que se ingresa ESC
-
-while (clave != "ESC") {
-    switch (clave) {
-        case "ARTESANIAS":
-            alert("¡Enhorabuena! Acceso permitido.");
-
-            let numeroClientes = prompt("Por favor ingrese el número de clientes: ");
-            let respuesta = "";
-
-            for (let i = 1; i <= numeroClientes; i++) {
-                let nombre = prompt("Cliente " + i + ": ¿Cuál es tu nombre?");
-                let edad = prompt("Cliente " + i + ": ¿Cuál es tu edad?");
-                let articulos = prompt(
-                    "Cliente " + i + ": ¿Cuántos artículos deseas comprar?"
-                );
-
-                switch (true) {
-                    case edad < 18 && articulos < 2:
-                        respuesta +=
-                            "Cliente " +
-                            i +
-                            ": Solo mayores de edad pueden comprar\n y Debes comprar mínimo 2 productos\n";
-                        break;
-                    case edad < 18 && articulos > 2:
-                        respuesta +=
-                            "Cliente " + i + ": Solo mayores de edad pueden comprar\n";
-                        break;
-                    case edad > 18 && articulos < 2:
-                        respuesta +=
-                            "Cliente " + i + ": Debes comprar mínimo 2 productos\n";
-                        break;
-                    default:
-                        respuesta += "Cliente " + i + ": Gracias por tu información\n";
-                }
-            }
-
-            alert(respuesta);
-
-            break;
-        default:
-            alert("Contraseña incorrecta");
-            break;
-    }
-
-    clave = prompt("Ingrese la contraseña");
+while (clave !== "ARTESANIAS") {
+    clave = prompt("Contraseña incorrecta. Ingresa la contraseña: ");
 }
+
+let nombre = prompt("¿Cuál es tu nombre?");
+let edad = prompt("¿Cuál es tu edad?");
+let articulos = prompt("¿Cuántos artículos deseas comprar?");
+
+let respuesta = "";
+
+if (edad < 18 && articulos < 2) {
+    respuesta = "Solo mayores de edad pueden comprar y Debes comprar mínimo 2 productos";
+} else if (edad < 18) {
+    respuesta = "Solo mayores de edad pueden comprar";
+} else if (articulos < 2) {
+    respuesta = "Debes comprar mínimo 2 productos";
+} else {
+    respuesta = "¡Felicidades! tienes un bono del 10%";
+}
+
+alert(respuesta);
