fa2ef96e30f1796ac297d50fdca687df22686c8d
karolrperdomo <karol.rperdomo@gmail.com>
2023-09-27 16:35:51 -0500

    se actualiza la funcion

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
-let clave = prompt("Bienvenido al apartado de Artesanias\nIngresa la contraseña: ");
+function comprarArtesanias() {
+    let clave = prompt("Bienvenido al apartado de Artesanias\nIngresa la contraseña: ");
 
-while (clave !== "ARTESANIAS") {
-    clave = prompt("Contraseña incorrecta. Ingresala nuevamente: ");
+    while (clave !== "ARTESANIAS") {
+        clave = prompt("Contraseña incorrecta. Ingrésala nuevamente: ");
+    }
+
+    let nombre = prompt("¿Cuál es tu nombre?");
+    let edad = prompt("¿Cuál es tu edad?");
+    let articulos = prompt("¿Cuántos artículos deseas comprar?");
+
+    let respuesta = "";
+
+    if (edad < 18 && articulos < 2) {
+        respuesta = "Solo mayores de edad pueden comprar y debes comprar mínimo 2 productos";
+    } else if (edad < 18) {
+        respuesta = "Solo mayores de edad pueden comprar";
+    } else if (articulos < 2) {
+        respuesta = "Debes comprar mínimo 2 productos";
+    } else {
+        respuesta = "¡Felicidades! tienes un bono del 10%";
+    }
+
+    alert(respuesta);
+
 }
 
-let nombre = prompt("¿Cuál es tu nombre?");
-let edad = prompt("¿Cuál es tu edad?");
-let articulos = prompt("¿Cuántos artículos deseas comprar?");
+console.log("Gracias por visitar nuestra página");
 
-let respuesta = "";
 
-if (edad < 18 && articulos < 2) {
-    respuesta = "Solo mayores de edad pueden comprar y Debes comprar mínimo 2 productos";
-} else if (edad < 18) {
-    respuesta = "Solo mayores de edad pueden comprar";
-} else if (articulos < 2) {
-    respuesta = "Debes comprar mínimo 2 productos";
-} else {
-    respuesta = "¡Felicidades! tienes un bono del 10%";
-}
 
-alert(respuesta);
+// Llama a la función para ejecutar el código
+comprarArtesanias();
